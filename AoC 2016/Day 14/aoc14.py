c2896273b950da30b8e51e8291e338e57e5af6e5
prot <tomastecko@gmail.com>
2023-11-24 10:50:02 +0100

    16.14-16.15

--- AoC 2016/Day 14/aoc14.py
+++ AoC 2016/Day 14/aoc14.py
@@ -1,8 +1,43 @@
+# 2023-11-24
+#Start	08:41
+#Part1	09:32	51min
+#Part2	09:55	23min
+#Total	74min
+import hashlib, itertools
 
-#Start	
-#Part1	
-#Part2	
-#Total
-with open('input') as f:
-	lista=f.read()
+def solve(s,gold=False):
+	c1=[]
+	c2=set()
+	i=0
+	while True:
+		initial=s+str(i)
+		if gold:
+			phrase=initial
+			for _ in range(2017):
+				phrase=hashlib.md5(phrase.encode()).hexdigest()
+		else:
+			phrase=hashlib.md5(initial.encode()).hexdigest()
+		test=[list(g) for k, g in itertools.groupby(phrase)]
+		# print(test)
+		# first=False
+		c5=[t[0] for t in test if len(t)>=5]
+		if len(c5)>=0:
+			for quint in c5:
+				for c in c1:
+					if i-1000<=c[0] and c[1]==quint[0]:
+						c2.add(c)
+						print(len(c2), c)
+		for t in test:
+			if len(t)>=3:
+				c1.append((i,t[0]))
+				break
+		if len(c2)>=64:
+			break
+		i+=1
 
+	print(sorted(list(c2))[63])
+
+s='yjdafjpo'
+# s='abc'
+solve(s)
+solve(s,True)
--- AoC 2016/Day 15/aoc15.py
+++ AoC 2016/Day 15/aoc15.py
@@ -1,8 +1,47 @@
+# 2023-11-24
+#Start	10:03
+#Part1	10:39	36min
+#Part2	10:43	4min
+#Total	40min
+import copy
 
-#Start	
-#Part1	
-#Part2	
-#Total
 with open('input') as f:
-	lista=f.read()
+	lista=f.read().splitlines()
 
+slots=[]
+for l in lista:
+	l=l.split()
+	slots.append([int(l[-1][:-1]),int(l[3])-1])
+
+# move all slots forward
+def increment(slots):
+	for i,s in enumerate(slots):
+		slots[i][0]=(slots[i][0]+1)%(slots[i][1]+1)
+	return slots
+
+# configuration where slots fall in line one by one
+def desired(slots):
+	slots2=[]
+	for i, v in enumerate(slots):
+		desired=([v[1]-i%(v[1]+1),v[1]])
+		slots2.append(desired)
+	return slots2
+
+def solve(slots,gold=False):
+	# lists fuck up otherwise
+	slots=copy.deepcopy(slots)
+	if gold:
+		slots.append([0,10])
+	slots2=desired(slots)
+	
+	j=0
+	while True:
+		if slots==slots2:
+			print(j)
+			break
+		else:
+			increment(slots)
+			j+=1
+
+solve(slots)
+solve(slots,True)
