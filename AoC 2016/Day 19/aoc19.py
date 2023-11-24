cbcf6df5d6055796d86cfbafaa687334596da8f2
unknown <tomastecko@gmail.com>
2023-11-24 18:11:36 +0100

    57%, 16.19,16.20

--- AoC 2016/Day 19/aoc19.py
+++ AoC 2016/Day 19/aoc19.py
@@ -1,8 +1,50 @@
+# 2023-11-24
+#Start	15:17
+#Part1	15:55	38min
+#Part2	17:22 	87min
+#Total	125min
+# n=5
 
-#Start	
-#Part1	
-#Part2	
-#Total
-with open('input') as f:
-	lista=f.read()
 
+def solve(n):
+	e=[[i,1] for i in range(1,n+1)]
+	while True:
+		# print(e)
+		# print(e)
+		if len(e)==2:
+			print(e)
+			print(e[0][0],e[0][1]+e[1][1])
+			break
+		odd=False
+		if len(e)%2!=0:
+			odd=True
+		if odd:
+			last=e.pop()
+		x1=e[::2]
+		x2=e[1::2]
+		test=list(zip(x1,x2))
+		test=[[t[0][0],t[0][1]+t[1][1]] for t in test]
+		# print(test)
+		if odd:
+			e=[last]+test
+		else:
+			e=test
+		print()
+solve(5)
+
+# takes 70 blasted minutes to solve, can't figure out another solution
+def solve2(n):
+	e=[[i,1] for i in range(1,n+1)]
+
+	while len(e)!=1:
+		if len(e)%1000==0:
+			print(len(e))
+		current=e[0]
+		d=len(e)//2
+		opposite=e.pop(d)
+		current=[current[0],current[1]+opposite[1]]
+		e.pop(0)
+		e.append(current)
+	print(e)
+
+solve2(3017957)
--- AoC 2016/Day 20/aoc20.py
+++ AoC 2016/Day 20/aoc20.py
@@ -1,8 +1,54 @@
-
-#Start	
-#Part1	
-#Part2	
-#Total
+# 2023-11-24
+#Start	17:26
+#Part1	17:59	33min
+#Part2	18:03	4min
+#Total	37min
 with open('input') as f:
-	lista=f.read()
+	lista=f.read().splitlines()
 
+bans=[]
+for l in lista:
+	l=l.split('-')
+	bans.append([int(l[0]),int(l[1])])
+
+# print(bans)
+
+while True:
+	start=len(bans)
+	# print(start)
+	reduced=False
+	for i,v1 in enumerate(bans):
+		for j, v2 in enumerate(bans):
+			new=None
+			if v1==v2:
+				continue
+			# extend lower range:
+			if v2[0]<=v1[0] and v1[0]<=v2[1]<=v1[1]-1:
+				# print('lower range')
+				new=[v2[0],v1[1]]
+			elif v1[1]+1>=v2[0]>=v1[0] and v2[1]>=v1[1]:
+				# print('higher range')
+				new=[v1[0],v2[1]]
+			elif v2[0]<=v1[0] and v2[1]>=v1[1]:
+				# print('encapsulates')
+				new=[v2[0],v2[1]]
+			elif v2[0]>v1[0] and v2[1]<v1[1]:
+				# print('within')
+				new=v1
+			else:
+				continue
+			if new!=None:
+				# print(f'merged {v1} with {v2} into {new}')
+				bans[i]=new
+				bans.pop(j)
+				reduced=True
+				break
+		if reduced:
+			break
+	if len(bans)==start:
+		break
+
+silver=sorted(bans)
+print('Silver: ',silver[0][1]+1)
+bans=[x[1]-x[0]+1 for x in bans]
+print('Gold: ',4294967296-sum(bans))
