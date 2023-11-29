07bcd2ec8e0534c57e422351daf33a0408a032cc
unknown <tomastecko@gmail.com>
2023-11-29 13:09:10 +0100

    60%, 16.13-16.14

--- AoC 2020/Day 13/aoc13.py
+++ AoC 2020/Day 13/aoc13.py
@@ -1,8 +1,85 @@
-
-#Start	
-#Part1	
-#Part2	
+# 2023-11-29
+#Start	11:22
+#Part1	11:29 	7min
+#Part2	X
 #Total
-with open('input') as f:
-	lista=f.read()
+n=1002578
+x=None
+t=[19,x,x,x,x,x,x,x,x,x,x,x,x,37,x,x,x,x,x,751,x,29,x,x,x,x,x,x,x,x,x,x,13,x,x,x,x,x,x,x,x,x,23,x,x,x,x,x,x,x,431,x,x,x,x,x,x,x,x,x,41,x,x,x,x,x,x,17]
 
+
+bus1=[t2 for t2 in t if t2!=None]
+print(bus1)
+
+
+n2=n
+found=False
+while True:
+	for b in bus1:
+		if n2%b==0:
+			print(n2, n2-n, b, (n2-n)*b)
+			found=1
+			break
+	if found:
+		break
+	n2+=1
+
+t2=[7,13,x,x,59,x,31,19]
+t=t2
+bus2=[(i,v) for i,v in enumerate(t) if v!=None]
+print(bus2)
+n3=0
+
+# something with lowest common divisors
+while True:
+	if n3%1000000==0:
+		print(n3)
+	found=True
+	for i,v in bus2:
+		if (n3+i)%v!=0:
+			found=False
+			break
+	if found:
+		print('found',n3)
+		for i,v in bus2:
+			print(i,v, n3%v)
+		break
+	n3+=t2[0]
+
+# n3=0
+# while True:
+# 	if n3%1000000==0:
+# 		print(n3)
+# 	found=True
+# 	for i,v in enumerate(t2):
+# 		if v==None:
+# 			continue
+# 		elif (n3+i)%v!=0:
+# 			found=False
+# 			break
+# 	if found:
+# 		print('found',n3)
+# 		break
+# 	n3+=t2[0]
+
+
+# n=0
+# increment=1
+# max_sync=0
+# while True:
+# 	print(n)
+# 	sync=0
+# 	for i,v in bus2:
+# 		if (n+i)%v==0:
+# 			sync+=1
+# 			if sync>max_sync and n>increment:
+# 				increment=n
+# 				max_sync=sync
+# 				print('\t new increment',increment,sync)
+# 		else:
+# 			found=False
+# 			break
+# 	if found:
+# 		print('found',n3)
+# 		break
+# 	n+=increment
--- AoC 2020/Day 14/aoc14.py
+++ AoC 2020/Day 14/aoc14.py
@@ -1,8 +1,60 @@
-
-#Start	
-#Part1	
-#Part2	
-#Total
+# 2023-11-29
+#Start	12:06
+#Part1	12:29	23min
+#Part2	13:04	35min
+#Total	58min
+import re, itertools, copy
 with open('input') as f:
-	lista=f.read()
+	lista=f.read().splitlines()
 
+test=lista
+
+silver={}
+for t in test:
+	if t[:2]=='ma':
+		entry=t[7:]
+		mask = [(i,v) for i,v in enumerate(entry) if v!='X']
+
+	elif t[:2]=='me':
+		entry=re.match(r'mem\[(\d+)\] = (\d+)', t)
+		entry=entry.groups()
+		test2=bin(int(entry[1]))[2:]
+		test2='0'*(36-len(test2))+test2
+		test2=list(test2)
+		for i,v in mask:
+			test2[i]=v
+		test2=''.join(test2)
+		result=int(test2,2)
+		silver[entry[0]]=result
+print('Silver:',sum(silver.values()))
+
+gold={}
+for t in test:
+	if t[:2]=='ma':
+		entry=t[7:]
+		mask = list(entry)
+
+	elif t[:2]=='me':
+		entry=re.match(r'mem\[(\d+)\] = (\d+)', t)
+		entry=entry.groups()
+		test2=bin(int(entry[0]))[2:]
+		test2='0'*(36-len(test2))+test2
+		test2=list(test2)
+		floating=0
+		for i,v in enumerate(mask):
+			if v=='0':
+				continue
+			elif v=='1':
+				test2[i]='1'
+			elif v=='X':
+				floating+=1
+				test2[i]='x'+str(floating)
+		poss=itertools.product([0,1], repeat=floating)
+		for p in poss:
+			address=copy.deepcopy(test2)
+			for i2,v2 in enumerate(p):
+				address[address.index(f'x{i2+1}')]=str(v2)
+			address=''.join(address)
+			address2=int(address,2)
+			gold[address2]=int(entry[1])
+print('Gold:',sum(gold.values()))
