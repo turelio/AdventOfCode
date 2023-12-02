136d63669844625786269f8355cdb4cc442b7d6d
unknown <tomastecko@gmail.com>
2023-12-02 12:24:18 +0100

    57.6%, 18.11 optimization

--- AoC 2018/Day 11/aoc11.py
+++ AoC 2018/Day 11/aoc11.py
@@ -1,8 +1,37 @@
+# 2023-12-02
+#Start	10:59	
+#Part1	11:17	18min
+#Part2	11:34	17min
+#Total	35min
 
-#Start	
-#Part1	
-#Part2	
-#Total
-with open('input') as f:
-	lista=f.read()
+# optimized from 40s to 4s by replacing itertools product used to generate square coordinates with manually summing rows in pre-calculated list
+x,y,n=300,300,7803
+# row/col 0 is unused
+p_list=[[None]*(x+1) for _ in range(y+1)]
+silver=[0,None]
+gold=[0,None]
 
+# pre-calculating
+for i in range(1, x+1):
+	for j in range(1, y+1):
+		rack_id=i+10
+		result=rack_id*j+n
+		result*=rack_id
+		result=(result//100)%10
+		result-=5
+		p_list[i][j]=result
+
+# 17x17 is the biggest possible
+for size in range(2,18):
+	print(f'{size:2}x{size}')
+	for i in range(1, x+2-size):
+		for j in range(1, y+2-size):
+			power=0
+			for j2 in range(j,j+size):
+				power+=sum(p_list[j2][i:i+size])
+			if power>gold[0]:
+				gold=[power, (i, j,size)]
+			if size==3 and power>silver[0]:
+				silver=gold=[power, (i, j,size)]
+
+print(silver,gold)
