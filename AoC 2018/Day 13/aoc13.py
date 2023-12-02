ae2c7ead7519f5ad0e0152d880af00e0a02c3ee1
unknown <tomastecko@gmail.com>
2023-12-02 18:12:39 +0100

    58.4%, 19.06

--- AoC 2018/Day 13/aoc13.py
+++ AoC 2018/Day 13/aoc13.py
@@ -1,8 +1,33 @@
-
-#Start	
-#Part1	
+# 2023-12-02
+#Start	X
+#Part1	X
 #Part2	
 #Total
-with open('input') as f:
-	lista=f.read()
+with open('input2') as f:
+	lista=f.read().splitlines()
+import copy
+# lista=lista.replace('\\', 'O').replace('/', 'O').split()
+print(lista)
+for l in lista:
+	print(len(l))
 
+[print(l) for l in lista]
+tick=1
+roads=['-','+','|']
+dirs=['>','v','<','^']
+corners=['\\','/']
+moves={'>':(1,0),'v':(0,-1),'<':(-1,0),'^':(0,1)}
+carts=[]
+for y in range(len(lista)):
+	for x in range(len(lista[0])):
+		if lista[y][x] in dirs:
+			carts.append(((y,x), dirs.index(lista[y][x])))
+			print(f'found cart at {x,y}')
+while True:
+	print(tick)
+	carts2=[]
+	carts=list(sorted(carts, key=lambda x:x[0]))
+	print(f'cart queue: {carts}')
+
+	tick+=1
+	break
--- AoC 2019/Day 6/aoc6.py
+++ AoC 2019/Day 6/aoc6.py
@@ -1,8 +1,53 @@
-
-#Start	
-#Part1	
-#Part2	
-#Total
+# 2023-12-02
+#Start	17:12
+#Part1	16:34	22min
+#Part2	17:53	19min
+#Total	41min
 with open('input') as f:
-	lista=f.read()
+	lista=f.read().splitlines()
 
+orbits={}
+for l in lista:
+	l=l.split(')')
+	if l[0] not in orbits:
+		orbits[l[0]]=[]
+	orbits[l[0]].append(l[1])
+
+silver=[]
+def solve(p,indirect=0):
+	if p=='COM':
+		direct=0
+	else:
+		direct=1
+	silver.append(direct+indirect)
+	if p in orbits:
+		for p2 in orbits[p]:
+			solve(p2,indirect+direct)
+solve('COM')
+
+
+orbits={}
+for l in lista:
+	l=l.split(')')
+	if l[0] not in orbits:
+		orbits[l[0]]=[]
+	if l[1] not in orbits:
+		orbits[l[1]]=[]
+	orbits[l[0]].append(l[1])
+	orbits[l[1]].append(l[0])
+
+visited=set()
+dist={k:10000 for k in orbits.keys()}
+dist['YOU']=0
+queue=['YOU']
+while len(queue)!=0:
+	c=queue.pop(0)
+	visited.add(c)
+	for move in orbits[c]:
+		if dist[c]+1<dist[move]:
+			dist[move]=dist[c]+1
+	moves=[m for m in orbits[c] if m not in visited and m not in queue]
+	queue+=moves
+
+print('Silver:',sum(silver))
+print('Gold:', dist['SAN']-2)
