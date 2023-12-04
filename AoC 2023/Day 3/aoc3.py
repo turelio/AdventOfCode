00773f3ff70cd97d196071c34995ddc8d72c9439
prot <tomastecko@gmail.com>
2023-12-04 06:37:48 +0100

    23.03, 23.04

--- AoC 2023/Day 3/aoc3.py
+++ AoC 2023/Day 3/aoc3.py
@@ -1,8 +1,39 @@
-# 
-# Start		
-# Part1		
-# Part2		
-# Total	
+# 2023-12-02
+# Start	06:00	
+# Part1	06:30	30min
+# Part2	06:43	13min
+# Total	43min
+# Rewrote without using itertools by looking for special characters instead of numbers' neighbors
+import re
 with open('input') as f:
-	lista=f.read()
+	lista=f.read().splitlines()
 
+silver,gold=0,0
+nums={}
+
+xr,yr=len(lista[0]), len(lista)
+for y,l in enumerate(lista):
+	test=re.finditer(r'(\d+)', l)
+	for number in test:
+		x1,x2 = number.span()
+		suma=int(number.group(0))
+		for t in range(x1,x2):
+			nums[(t,y)]=(x1,x2,y,suma)
+
+silver=set()
+
+gold=0
+for i,y in enumerate(lista):
+	for j,x in enumerate(y):
+		if x!='.' and not x.isdigit():
+			near=[(j-1,i-1),(j,i-1),(j+1,i-1),(j-1,i),(j+1,i),(j-1,i+1),(j,i+1),(j+1,i+1)]
+			found=set()
+			for n in near:
+				if n in nums:
+					silver.add(nums[n])
+					found.add(nums[n])
+			if x=='*' and len(found)==2:
+				found=list(found)
+				gold+=found[0][3]*found[1][3]
+silver=sum([s[3] for s in silver])
+print('Silver:',silver,'Gold:',gold)
--- AoC 2023/Day 4/aoc4.py
+++ AoC 2023/Day 4/aoc4.py
@@ -1,8 +1,25 @@
-# 
-# Start		
-# Part1		
-# Part2		
-# Total	
+# 2023-12-03
+# Start	06:00	
+# Part1	06:08	8min
+# Part2	06:23	15min
+# Total	23min
+import re
 with open('input') as f:
-	lista=f.read()
+	lista=f.read().splitlines()
 
+silver=0
+counts={i:1 for i,v in enumerate(lista)}
+for i,l in enumerate(lista):
+	entry=re.split('[:|]', l)
+	n, win, candidates=entry
+	win=re.split(r'\s+', win.strip())
+	candidates=re.split(r'\s+', candidates.strip())
+	x=len(set(candidates).intersection(set(win)))
+	if x>0:
+		i2=min(i+x+1, len(lista))
+		for j in range(i+1,i2):
+			counts[j]+=counts[i]
+		silver+=2**(x-1)
+
+print('Silver:',silver)
+print('Gold:',sum(counts.values()))
