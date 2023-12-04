21742b7c3f5a4d9821d2a3f9cbc6fdc51648acb6
prot <tomastecko@gmail.com>
2023-12-04 06:59:39 +0100

    23.04 refactor

--- AoC 2023/Day 4/aoc4.py
+++ AoC 2023/Day 4/aoc4.py
@@ -3,18 +3,18 @@
 # Part1	06:08	8min
 # Part2	06:23	15min
 # Total	23min
-import re
 with open('input') as f:
 	lista=f.read().splitlines()
 
 silver=0
-counts={i:1 for i,v in enumerate(lista)}
+counts=[1]*len(lista)
 for i,l in enumerate(lista):
-	entry=re.split('[:|]', l)
-	n, win, candidates=entry
-	win=re.split(r'\s+', win.strip())
-	candidates=re.split(r'\s+', candidates.strip())
-	x=len(set(candidates).intersection(set(win)))
+	test=l.replace('  ',' ')
+	test=test.split(':')[1]
+	win, candidates=test.split('|')
+	win=set(win.strip().split(' '))
+	candidates=set(candidates.strip().split(' '))
+	x=len(win&candidates)
 	if x>0:
 		i2=min(i+x+1, len(lista))
 		for j in range(i+1,i2):
@@ -22,4 +22,4 @@ for i,l in enumerate(lista):
 		silver+=2**(x-1)
 
 print('Silver:',silver)
-print('Gold:',sum(counts.values()))
+print('Gold:',sum(counts))
