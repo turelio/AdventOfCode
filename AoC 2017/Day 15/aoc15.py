959db59c6c1a2a0dafb2337a9d651303d71a8de1
unknown <tomastecko@gmail.com>
2023-12-04 12:05:54 +0100

    61.1%, 17.15, 18.18, 19.08

--- AoC 2017/Day 15/aoc15.py
+++ AoC 2017/Day 15/aoc15.py
@@ -1,8 +1,38 @@
+# 2023-12-04
+#Start	10:15
+#Part1	10:25	10min
+#Part2	10:32	7min
+#Total	17min
 
-#Start	
-#Part1	
-#Part2	
-#Total
-with open('input') as f:
-	lista=f.read()
+a=289
+b=629
+# a=65
+# b=8921
+fa=16807
+fb=48271
+d=2147483647
+# silver=0
+# for i in range(40000000):
+# 	if i%1000000==0:
+# 		print(i)
+# 	a=(a*fa)%d
+# 	b=(b*fb)%d
+# 	if a&0xffff==b&0xffff:
+# 		silver+=1
 
+# print(silver)
+gold=0
+i=0
+while i<5000000:
+	if i%100000==0:
+		print(i)
+	a=(a*fa)%d
+	while a%4!=0:
+		a=(a*fa)%d
+	b=(b*fb)%d
+	while b%8!=0:
+		b=(b*fb)%d
+	if a&0xffff==b&0xffff:
+		gold+=1
+	i+=1
+print(gold)
--- AoC 2019/Day 8/aoc8.py
+++ AoC 2019/Day 8/aoc8.py
@@ -1,8 +1,33 @@
-
-#Start	
-#Part1	
-#Part2	
-#Total
+# 2023-12-04
+#Start	11:20
+#Part1	11:29	9min
+#Part2	11:47	18min
+#Total	27min
+from textwrap import wrap
 with open('input') as f:
-	lista=f.read()
+	lista=f.read().strip()
 
+x,y=25, 6
+# wrap splits every nth character
+layers=wrap(lista,(x*y))
+silver=[1000000,None]
+
+for l in layers:
+	if l.count('0')<silver[0]:
+		silver=[l.count('0'),l]
+
+layers=[wrap(l,x) for l in layers]
+screen=[[None]*x for _ in range(y)]
+
+for y1 in range(y):
+	for x1 in range(x):
+		for layer in layers:
+			if layer[y1][x1] in ['0','1']:
+				screen[y1][x1]=layer[y1][x1]
+				break
+
+print('Silver:',silver[1].count('1')*silver[1].count('2'))
+print('Gold:')
+for s in screen:
+	s=['█' if s2=='1' else ' ' for s2 in s]
+	print(''.join(s))
