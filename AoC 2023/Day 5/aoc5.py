d555385633c33e4845bc6595e44a5a6dd86a2b0a
unknown <tomastecko@gmail.com>
2023-12-05 16:54:27 +0100

    23.05 rewrites

--- AoC 2023/Day 5/aoc5.py
+++ AoC 2023/Day 5/aoc5.py
@@ -3,60 +3,55 @@
 # Part1	06:26	26min
 # Part2	10:47	~244min
 # Total	270min
+# tried to do range splitting before giving up and bruteforcing from reverse
 with open('input') as f:
 	lista=f.read().strip().split('\n\n')
-
+# parse
 seeds=lista[0].split()[1:]
 seeds=[int(l) for l in seeds]
 maps=lista[1:]
 maps=[m.split('\n') for m in maps]
 maps=[m[1:] for m in maps]
-maps=[[m2.split() for m2 in m] for m in maps]
-maps=[[list(map(int,m2)) for m2 in m] for m in maps]
-maps2=maps[::-1]
+maps=[[list(map(int,m2.split())) for m2 in m] for m in maps]
 
-#input to output
-def tf1(seed):
-	for i,map in enumerate(maps):
-		seed2=seed
-		for ranges in map:
+# seed to location
+def to_location(seed):
+	for layer in maps:
+		for ranges in layer:
 			if seed in range(ranges[1],ranges[1]+ranges[2]):
 				seed=seed+(ranges[0]-ranges[1])
 				break
 	return seed
-# output to input
-def tf2(seed):
-	for map in maps2:
-		seed2=seed
-		for ranges in map:
-			if seed in range(ranges[0],ranges[0]+ranges[2]):
-				seed=seed-(ranges[0]-ranges[1])
-				break
-	return seed
 
+# location to seed
+def to_seed(location):
+	for layer in maps[::-1]:
+		for ranges in layer:
+			if location in range(ranges[0],ranges[0]+ranges[2]):
+				location-=(ranges[0]-ranges[1])
+				break
+	return location
+
+# Part 1
 silver=[]
 for s in seeds:
-	silver.append(tf1(s))
-
+	silver.append(to_location(s))
 print('Silver:',min(silver))
-gold=[]
-for i in range(0,len(seeds),2):
-	gold.append(range(seeds[i],seeds[i]+seeds[i+1]))
-answer=False
-i=0
+
+# Part 2 bruteforce
+ranges=[range(seeds[i],seeds[i]+seeds[i+1]) for i in range(0,len(seeds),2)]
+gold=10**10
 mod=10000
-answer=1000000000000
-end=0
+i, end=0,0
 while i>=end:
-	test=tf2(i)
-	for g in gold:
+	test=to_seed(i)
+	for g in ranges:
 		if test in g:
-			if i<answer:
-				answer=i
-				# print('new lowest',i)
+			if i<gold:
+				gold=i
 			mod=-1
 			end=i-10000
 			break
 	i+=mod
-print('Gold:',answer)
+print('Gold:', gold)
 
