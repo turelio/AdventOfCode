3f99827f96991b4f70658b27052ef086f283898b
prot <tomastecko@gmail.com>
2023-12-06 06:22:29 +0100

    62.7%, 23.06

--- AoC 2023/Day 6/aoc6.py
+++ AoC 2023/Day 6/aoc6.py
@@ -1,8 +1,22 @@
-# 
-# Start		
-# Part1		
-# Part2		
-# Total	
-with open('input') as f:
-	lista=f.read()
+# 2023-12-06
+# Start	06:00
+# Part1	06:10	10min
+# Part2	06:11	1min
+# Total	11min
 
+silver=[(62,553),(64,1010),(91,1473),(90,1074)]
+gold=[(62649190,553101014731074)]
+
+def solve(times):
+	total=1
+	for d, record in times:
+		won=0
+		for i in range(d+1):
+			if (i*(d-i))>record:
+				won+=1
+				pass
+		total*=won
+	return total
+
+print('Silver:',solve(silver))
+print('Gold:',solve(gold))
