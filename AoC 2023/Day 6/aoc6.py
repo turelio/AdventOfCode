2c68d17204d315a8f7b7c7aa969667a88d1fbddc
unknown <tomastecko@gmail.com>
2023-12-07 11:21:29 +0100

    23.06 optimization

--- AoC 2023/Day 6/aoc6.py
+++ AoC 2023/Day 6/aoc6.py
@@ -3,20 +3,20 @@
 # Part1	06:10	10min
 # Part2	06:11	1min
 # Total	11min
-
+import math
 silver=[(62,553),(64,1010),(91,1473),(90,1074)]
 gold=[(62649190,553101014731074)]
 
+# replaced bruteforce with quadratic equation, using discriminant and roots
 def solve(times):
 	total=1
+	# 0 = -x^2 + dx - record, discriminant = sqrt(d^2-4*record)
 	for d, record in times:
-		won=0
-		for i in range(d+1):
-			if (i*(d-i))>record:
-				won+=1
-				pass
-		total*=won
+		x1=math.ceil((-d + math.sqrt(d**2-4*record))/-2)
+		x2=math.floor((-d - math.sqrt(d**2-4*record))/-2)
+		total*=x2-x1+1
 	return total
 
 print('Silver:',solve(silver))
 print('Gold:',solve(gold))
+
