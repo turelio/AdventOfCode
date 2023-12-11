3b86e21b85496892e6808826f50aa093826f6fd3
prot <tomastecko@gmail.com>
2023-12-11 09:37:32 +0100

    64.7%, 23.10-23.11

--- AoC 2023/Day 10/aoc10.py
+++ AoC 2023/Day 10/aoc10.py
@@ -1,8 +1,133 @@
-# 
-# Start		
-# Part1		
-# Part2		
+# 2023-12-10
+# Start	09:01	
+# Part1	09:33	32min
+# Part2	X	+48min
 # Total	
 with open('input') as f:
-	lista=f.read()
+	lista=f.read().splitlines()
+import copy
+# |
+test='''.....
+.S-7.
+.|.|.
+.L-J.
+.....'''.splitlines()
+# lista=test
+for y,v in enumerate(lista):
+	lista[y]=list(lista[y])
+	# print(lista)
+	if 'S' in v:
+		c=[v.index('S'), y]
+		print(c)
+		lista[y][v.index('S')]='|'
 
+# [print(l) for l in lista]
+start=copy.deepcopy(c)
+prev='U'
+c=start
+pipes=set()
+while True:
+	# print(f'at {c} = {lista[c[1]][c[0]]}')
+	stop=lista[c[1]][c[0]]
+	pipes.add(tuple(c))
+	if stop =='-':
+		if prev=='R':
+			c=[c[0]+1,c[1]]
+			prev='R'
+		elif prev=='L':
+			c=[c[0]-1,c[1]]
+			prev='L'
+	elif stop =='|':
+		if prev=='D':
+			c=[c[0],c[1]+1]
+			prev='D'
+		elif prev=='U':
+			c=[c[0],c[1]-1]
+			prev='U'
+	elif stop =='7':
+		if prev=='R':
+			c=[c[0],c[1]+1]
+			prev='D'
+		elif prev=='U':
+			c=[c[0]-1,c[1]]
+			prev='L'
+		else:
+			print('invalid')
+			break
+	elif stop =='J':
+		if prev=='D':
+			c=[c[0]-1,c[1]]
+			prev='L'
+		elif prev=='R':
+			c=[c[0],c[1]-1]
+			prev='U'
+		else:
+			print('invalid')
+			break
+	elif stop =='L':
+		if prev=='L':
+			c=[c[0],c[1]-1]
+			prev='U'
+		elif prev=='D':
+			c=[c[0]+1,c[1]]
+			prev='R'
+		else:
+			print('invalid')
+			break
+	elif stop =='F':
+		if prev=='U':
+			c=[c[0]+1,c[1]]
+			prev='R'
+		elif prev=='L':
+			c=[c[0],c[1]+1]
+			prev='D'
+		else:
+			print('invalid')
+			break
+	else:
+		print(f'{stop} not specified')
+		break
+	if c==start:
+		break
+
+print(len(pipes)//2)
+print(len(lista),len(lista[0]))
+yr=len(lista)
+xr=len(lista[0])
+lista2=copy.deepcopy(lista)
+for y,v in enumerate(lista2):
+	for x,v2 in enumerate(v):
+		if (x,y) not in pipes:
+			lista2[y][x]='.'
+
+
+
+# remove outer flood
+outer=set()
+outer.add((0,0))
+visited=set()
+while True:
+	old=len(outer)
+	for x,y in outer:
+		if (x,y) in visited:
+			continue
+		near=[(x-1,y),(x+1,y),(x-1,y-1),(x,y-1),(x+1,y-1),(x-1,y+1),(x,y+1),(x+1,y+1)]
+		near=[n for n in near if n[1] in range(xr) and n[0] in range(yr)]
+		near=[n for n in near if lista2[n[1]][n[0]]=='.']
+		# print(near)
+		outer=outer.union(set(near))
+		visited.add((x,y))
+	print(len(outer))
+	if len(outer)==old:
+		break
+
+for x,y in outer:
+	print(lista2[y][x])
+	lista2[y][x]=' '
+	print(lista2[y][x])
+
+[print(''.join(l)) for l in lista2]
+tiles=0
+for y in lista2:
+	tiles+=y.count('.')
+print(tiles)
--- AoC 2023/Day 11/aoc11.py
+++ AoC 2023/Day 11/aoc11.py
@@ -1,8 +1,102 @@
-# 
-# Start		
-# Part1		
-# Part2		
-# Total	
+# 23-12-11
+# Start	08:50	
+# Part1	09:03	13min
+# Part2	09:30	27min
+# Total	40min
 with open('input') as f:
-	lista=f.read()
+	lista=f.read().splitlines()
 
+test='''...#......
+.......#..
+#.........
+..........
+......#...
+.#........
+.........#
+..........
+.......#..
+#...#.....'''.splitlines()
+# lista=test
+lista=[list(l) for l in lista]
+# [print(''.join(l)) for l in lista]
+# print(lista)
+g3=[]
+for y,v in enumerate(lista):
+	for x,v2 in enumerate(v):
+		if lista[y][x]=='#':
+			g3.append([x,y])
+
+y=0
+ye=[]
+for y,v in enumerate(lista):
+	if '#' not in v:
+		ye.append(y)
+lista=list(zip(*lista))
+
+x=0
+xe=[]
+for x,v in enumerate(lista):
+	if '#' not in v:
+		xe.append(x)
+lista=list(zip(*lista))
+
+import copy
+print(g3,xe,ye)
+gal=[]
+for g in g3:
+	print(g)
+	entry=copy.deepcopy(g)
+	for x in xe:
+		if x<g[0]:
+			entry[0]+=999999
+	for y in ye:
+		if y<g[1]:
+			entry[1]+=999999
+	gal.append(entry)
+	print('\t',entry)
+
+import itertools
+pairs=list(itertools.combinations(gal,2))
+print(len(pairs))
+silver=0
+for g1,g2 in pairs:
+	silver+=abs(g1[0]-g2[0])+abs(g1[1]-g2[1])
+print(silver)
+
+
+
+# y=0
+# while y<len(lista):
+# 	if '#' in lista[y]:
+# 		y+=1
+# 	else:
+# 		lista.insert(y,lista[y])
+# 		y+=2
+# # print('part1')
+# # [print(''.join(l)) for l in lista]
+# lista=list(zip(*lista))
+# # [print(''.join(l)) for l in lista]
+# y=0
+# while y<len(lista):
+# 	if '#' in lista[y]:
+# 		y+=1
+# 	else:
+# 		lista.insert(y,lista[y])
+# 		y+=2
+# lista=list(zip(*lista))
+# g=[]
+# # [print(''.join(l)) for l in lista]
+# # print('part2')
+# for y,v in enumerate(lista):
+# 	for x,v2 in enumerate(v):
+# 		if lista[y][x]=='#':
+# 			g.append((x,y))
+# print(len(g))
+# import itertools
+# pairs=list(itertools.combinations(g,2))
+# print(len(pairs))
+# silver=0
+# for g1,g2 in pairs:
+# 	silver+=abs(g1[0]-g2[0])+abs(g1[1]-g2[1])
+# print(silver)
+# print(g3)
